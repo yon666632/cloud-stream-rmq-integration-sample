cd89e2020eeaa9468da765b26b5a9ba9df2b50c0
Suguru ONODA <s-onoda@mail.nissan.co.jp>
2019-05-10 22:19:09 +0900

    Add test

---
+++ sender/src/test/java/com/example/sender/SendMessageServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.sender;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.messaging.Message;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class SendMessageServiceTest {
+
+    @Autowired
+    SendMessageService service;
+
+    @Autowired
+    Source source;
+
+    @Autowired
+    MessageCollector collector;
+
+    @Test
+    public void sendTest() {
+        service.send();
+        Message<String> sendMessage =
+                (Message<String>) collector.forChannel(source.output()).poll();
+        Assertions.assertThat(sendMessage.getPayload()).isEqualTo("hello");
+    }
+}
